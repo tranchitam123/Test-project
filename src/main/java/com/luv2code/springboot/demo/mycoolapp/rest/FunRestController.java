3a9f8dc7b78ba4ec82998d271afbfdd7374fadd9
TranChiTam <tranchitam65a@gmail.com>
2024-08-23 17:11:59 +0700

    Update FunRestController

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunRestController {
     @GetMapping("/")
     public String sayHello(){
-        return "Hello World!";
+        return "Hello World my friend!!!!";
     }
 }
