8dbc8c6dfacefe8d0ee4b951eb1d85c74fa9dc86
Trần Chí Tâm <132425412+tranchitam123@users.noreply.github.com>
2024-08-23 17:13:44 +0700

    Update FunRestController.java
    add some word

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunRestController {
     @GetMapping("/")
     public String sayHello(){
-        return "Hello World my friend!!!!";
+        return "Hello World my friend!!!!"; //are you sure
     }
 }
